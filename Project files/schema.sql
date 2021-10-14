4692e0b6ce49fc3ff401c6a18edbb4292717b6ea
Hiong Kai Han <kaihan00000@gmail.com>
2021-10-15 02:58:24 +0800

    Add tables

---
+++ Project files/schema.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Sessions (
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE, /* Not sure what data type to use  */
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (room) REFERENCES meetingRooms (room) ON DELETE CASCADE,
+    FOREIGN KEY (floor) REFERENCES meetingRooms (floor) ON DELETE CASCADE,
+);
+
+CREATE TABLE Approves (
+    managerID INTEGER,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
+    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
+    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
+    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE,
+);
+
+CREATE TABLE Books (
+    bookerID INTEGER NOT NULL,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
+    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
+    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
+    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE,
+);
+
+CREATE TABLE Joins (
+    eid INTEGER NOT NULL,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
+    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
+    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
+    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE,
+);
