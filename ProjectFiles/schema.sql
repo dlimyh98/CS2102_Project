c15df111915f6f354011a438b425f28a663ed807
dlimyh98 <dlimyh98@hotmail.com>
2021-10-16 18:52:41 +0800

    fixed Sessions bug, file organization

--- ProjectFiles/schema.sql
+++ ProjectFiles/schema.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE Employees (
-    eid INTEGER,
+    eid BIGSERIAL,
     ename TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
+    email TEXT DEFAULT uuid_generate_v4(),
     resignedDate DATE DEFAULT '1001-01-01',
     PRIMARY KEY (eid)
 );
@@ -62,58 +64,6 @@ CREATE TABLE worksIn (
     FOREIGN KEY (did) REFERENCES Departments (did)
 );
 
-CREATE TABLE Sessions (
-    room INTEGER,
-    floor INTEGER,
-    date DATE,
-    time TIME WITHOUT TIME ZONE, /* Not sure what data type to use */
-    PRIMARY KEY (room, floor, date, time),
-    FOREIGN KEY (room) REFERENCES meetingRooms (room) ON DELETE CASCADE,
-    FOREIGN KEY (floor) REFERENCES meetingRooms (floor) ON DELETE CASCADE,
-);
-
-CREATE TABLE Approves (
-    managerID INTEGER,
-    room INTEGER,
-    floor INTEGER,
-    date DATE,
-    time TIME WITHOUT TIME ZONE,
-    PRIMARY KEY (room, floor, date, time),
-    FOREIGN KEY (managerID) REFERENCES Manager (managerID),
-    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
-    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
-    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
-    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE,
-);
-
-CREATE TABLE Books (
-    bookerID INTEGER NOT NULL,
-    room INTEGER,
-    floor INTEGER,
-    date DATE,
-    time TIME WITHOUT TIME ZONE,
-    PRIMARY KEY (room, floor, date, time),
-    FOREIGN KEY (bookerID) REFERENCES Booker (bookerID) ON DELETE CASCADE,
-    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
-    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
-    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
-    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE,
-);
-
-CREATE TABLE Joins (
-    eid INTEGER NOT NULL,
-    room INTEGER,
-    floor INTEGER,
-    date DATE,
-    time TIME WITHOUT TIME ZONE,
-    PRIMARY KEY (eid, room, floor, date, time),
-    FOREIGN KEY (eid) REFERENCES Employees (eid) ON DELETE CASCADE,
-    FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE,
-    FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
-    FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE,
-    FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE
-);
-
 CREATE TABLE meetingRooms (
 	room INTEGER,
 	floor INTEGER,
@@ -138,4 +88,46 @@ CREATE TABLE Updates (
 	floor INTEGER,
 	PRIMARY KEY (date, room, floor),
 	FOREIGN KEY (room, floor) REFERENCES meetingRooms (room, floor)
+);
+
+CREATE TABLE Sessions (
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE, /* Not sure what data type to use */
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (room, floor) REFERENCES meetingRooms (room,floor) ON DELETE CASCADE
+);
+
+CREATE TABLE Approves (
+    managerID INTEGER,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (managerID) REFERENCES Manager (managerID),
+    FOREIGN KEY (room, floor, date, time) REFERENCES Sessions (room, floor, date, time) ON DELETE CASCADE
+);
+
+CREATE TABLE Books (
+    bookerID INTEGER NOT NULL,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (room, floor, date, time),
+    FOREIGN KEY (bookerID) REFERENCES Booker (bookerID) ON DELETE CASCADE,
+    FOREIGN KEY (room, floor, date, time) REFERENCES Sessions (room, floor, date, time) ON DELETE CASCADE
+);
+
+CREATE TABLE Joins (
+    eid INTEGER NOT NULL,
+    room INTEGER,
+    floor INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    PRIMARY KEY (eid, room, floor, date, time),
+    FOREIGN KEY (eid) REFERENCES Employees (eid) ON DELETE CASCADE,
+    FOREIGN KEY (room, floor, date, time) REFERENCES Sessions (room, floor, date, time) ON DELETE CASCADE
 );
